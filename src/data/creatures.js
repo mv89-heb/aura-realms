75009244074963dddf832037120c085ab22722b2
mv89-heb <valanimattan89@gmail.com>
2026-09-16 14:22:33 +0300

    feat: add initial creature data

---
+++ src/data/creatures.js
@@ -0,0 +1,8 @@
+export const CREATURES = {
+  lumiko: { id:'lumiko', name:'Lumiko', element:'lumen', color:0x8ee7ff, accent:0xffffff, hp:100, attack:18 },
+  mossli: { id:'mossli', name:'Mossli', element:'verdant', color:0x73d68a, accent:0xd9ffd9, hp:115, attack:16 },
+  aquini: { id:'aquini', name:'Aquini', element:'tide', color:0x58b7ff, accent:0xdff7ff, hp:105, attack:17 },
+  emberu: { id:'emberu', name:'Emberu', element:'ember', color:0xff8b4d, accent:0xffe0a3, hp:95, attack:21 },
+  voltin: { id:'voltin', name:'Voltin', element:'volt', color:0xffe45c, accent:0xfffff0, hp:92, attack:22 },
+  nocti: { id:'nocti', name:'Nocti', element:'shadow', color:0x9c7cff, accent:0xe4dcff, hp:98, attack:20 }
+};
